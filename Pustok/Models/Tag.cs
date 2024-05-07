d69d20ae886ba4c0c9e72fed03d04656e5254355
anarpi <anarpi@code.edu.az>
2024-05-07 18:26:13 +0400

    new

---
+++ Pustok/Models/Tag.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pustok.Models
+{
+
+    
+    public class Tag:BaseEntity
+    {
+        public string Name { get; set; }
+        public List<BookTag>? Tags { get; set; }
+    }
+}
